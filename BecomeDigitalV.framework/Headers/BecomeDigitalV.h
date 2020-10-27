8f5bba4df13bc8e851c6672094af62ac95ca8808
Nelson peña <nelson.pena@idemia.com>
2020-10-27 09:43:00 -0500

    SDK IOS
    Archivos integraciones SDK

---
+++ BecomeDigitalV.framework/Headers/BecomeDigitalV.h
@@ -0,0 +1,19 @@
+//
+//  BecomeDigitalV.h
+//  BecomeDigitalV
+//
+//  Created by Nelson Peña on 22/06/20.
+//  Copyright © 2020 Nelson Peña. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BecomeDigitalV.
+FOUNDATION_EXPORT double BecomeDigitalVVersionNumber;
+
+//! Project version string for BecomeDigitalV.
+FOUNDATION_EXPORT const unsigned char BecomeDigitalVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BecomeDigitalV/PublicHeader.h>
+
+
